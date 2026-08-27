967d0ad864d4737536981ca2421388ea136240a6
LilyTea <voilalesfraises@gmail.com>
2026-08-27 16:09:05 -0400

    Basic webpage template up

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
+from pydantic import BaseModel
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+#setting this using Pydantic, not entirely sure how this works
+class NameReq(BaseModel):
+    name : str
 
+#pointing to the html page that builds the webpage
+templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+#build webpage
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
@@ -16,8 +22,9 @@ def home(request: Request):
         context={}
     )
 
+#this is the output returned to be printed on the webpage
 @app.post("/hello")
-def greet(name : str = Form()):
-    return {"message" : f"Hello {name}"}
+def greet(data : NameReq):
+    return {"message" : f"Sup {data.name}"}
 
 # uvicorn app.main:app --reload
--- static/script.js
+++ static/script.js
@@ -1,11 +1,29 @@
+// confirm js is working
 console.log("Hello from JavaScript!");
+output.textContent = "JavaScript is active! :)";
 
-const output = document.getElementById("output");
-
-output.textContent = "JavaScript changed this!";
-
+// monitor button for clicks
 const button = document.getElementById("submission");
 
-button.addEventListener("click", function(){
-   console.log("Button clicked")
+// main event
+button.addEventListener("click", async function(){
+    // pull the str entered into the submission field
+    const input = document.getElementById("name").value
+    // print out input on page just to check
+    output.textContent = input
+    // send off data to main.py through FastAPI
+    const response = await fetch("/hello", {
+        method : "POST",
+        body : JSON.stringify({name : input}),
+        headers : {
+            "Content-Type" : "application/json"
+        }
+    })
+    // assign response
+    const data = await response.json()
+    // assign text to be rewritten
+    const greeting = document.getElementById("greeting")
+    // overwrite text on page
+    greeting.textContent = data.message
+    console.log("complete")
 });
